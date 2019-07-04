f64305e3eb73162b5efab3bb337fb6be8ac79203
dmitrijandrianov <garro93@gmail.com>
2019-07-04 23:06:40 +0400

    Initial Commit

---
+++ Events/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Events
+//
+//  Created by Дмитрий Андриянов on 04/07/2019.
+//  Copyright © 2019 Дмитрий Андриянов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
