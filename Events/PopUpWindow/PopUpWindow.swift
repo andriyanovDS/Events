5c8e714df1524eb78f93c7c50beef7060bbf74a3
soslankulumbekov <skulumbekov@wowworks.ru>
2019-07-28 21:34:55 +0400

    delete comment

--- Events/PopUpWindow/PopUpWindow.swift
+++ Events/PopUpWindow/PopUpWindow.swift
@@ -14,13 +14,6 @@ protocol PopUpDelegate {
 
 class PopUpWindow: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
     var delegate: PopUpDelegate?
     var coordinator: MainCoordinator?
    
