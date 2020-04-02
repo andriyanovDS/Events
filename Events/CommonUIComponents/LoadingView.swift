b9fea283ba94353ea060e2e7295737c92dc7704a
dmitriyandriyanov <garro93@gmail.com>
2020-04-02 15:52:29 +0300

    refactor loading view animation

--- Events/CommonUIComponents/LoadingView.swift
+++ Events/CommonUIComponents/LoadingView.swift
@@ -9,17 +9,12 @@
 import UIKit
 
 class LoadingView: UIView {
-	private var tasks: [DispatchWorkItem] = []
 	private var viewCenterX: CGFloat = CGFloat.zero
 	
 	private struct Constants {
 		static let animationDuration: TimeInterval = 600
 		static let circlesCount: Int = 3
 		static let circleRadius: CGFloat = 5.0
-	}
-	
-	deinit {
-		tasks.forEach { $0.cancel() }
 	}
 	
 	private func centerX(at index: Int) -> CGFloat {
@@ -32,37 +27,32 @@ class LoadingView: UIView {
 	override func draw(_ rect: CGRect) {
 		viewCenterX = rect.width / 2
 		let range = Array([Int](0...Constants.circlesCount - 1))
-		let layers = range
-			.enumerated()
-			.map {(index, _) in drawCircle(at: index)}
 		
-		tasks = layers
+		let animation = CAKeyframeAnimation(keyPath: "position")
+		animation.keyTimes = [0, 0.5, 1]
+		animation.duration = Constants.animationDuration / 1000
+		animation.repeatCount = Float.greatestFiniteMagnitude
+		animation.timingFunctions = [.linear]
+		
+		range
 			.enumerated()
-			.map { (index, layer) in
-				let task = DispatchWorkItem {[unowned self] in
-					layer.startDownAnimation()
-					if index == layers.count - 1 {
-						self.tasks = []
-					}
-				}
-				let delay = Int(Constants.animationDuration) / Constants.circlesCount * index
-				DispatchQueue.main.asyncAfter(
-					deadline: DispatchTime.now() + .milliseconds(delay),
-					execute: task
-				)
-				return task
-			}
+			.forEach {(index, _) in drawCircle(at: index, withAnimation: animation)}
 	}
 
-	private func drawCircle(at index: Int) -> LoadingViewLayer {
+	private func drawCircle(at index: Int, withAnimation animation: CAKeyframeAnimation) {
 		let startPoint = CGPoint(
 			x: centerX(at: index),
 			y: Constants.circleRadius
 		)
-		let circleLayer = LoadingViewLayer(
-			translationY: Constants.circleRadius * 3,
-			animationDuration: Constants.animationDuration / 1000
+		let endPoint = CGPoint(
+			x: startPoint.x,
+			y: Constants.circleRadius * 3
 		)
+		let delayInMilliseconds = Int(Constants.animationDuration) / Constants.circlesCount * index
+		animation.timeOffset = Double(delayInMilliseconds) / 1000
+		animation.values = [startPoint, endPoint, startPoint]
+		
+		let circleLayer = CAShapeLayer()
 		circleLayer.position = startPoint
 		let path = UIBezierPath(
 			arcCenter: CGPoint.zero,
@@ -75,75 +65,6 @@ class LoadingView: UIView {
 		circleLayer.fillColor = UIColor.black.cgColor
 		circleLayer.position = startPoint
 		self.layer.addSublayer(circleLayer)
-		return circleLayer
-	}
-}
-
-private class LoadingViewLayer: CAShapeLayer {
-	let translationY: CGFloat
-	let animationDuration: TimeInterval
-	private var activeAnimation: Animation = .down
-	private var animation: CABasicAnimation = {
-		let animation = CABasicAnimation(keyPath: "transform")
-		animation.timingFunction = .linear
-		return animation
-	}()
-	
-	private enum Animation {
-		case up, down
-	}
-
-	init(translationY: CGFloat, animationDuration: TimeInterval) {
-		self.translationY = translationY
-		self.animationDuration = animationDuration
-		super.init()
-		animation.delegate = self
-		animation.duration = animationDuration
-	}
-	
-	override init(layer: Any) {
-		guard let layer = layer as? LoadingViewLayer else {
-			fatalError("Unexpected layer")
-		}
-		translationY = layer.translationY
-		animationDuration = layer.animationDuration
-		super.init(layer: layer)
-		animation.delegate = self
-		animation.duration = animationDuration
-	}
-	
-	required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
-	
-	func startDownAnimation() {
-		let animation = self.animation
-		let transform = CATransform3DMakeTranslation(1, translationY, 1)
-		animation.fromValue = CATransform3DIdentity
-		animation.toValue = transform
-		activeAnimation = .down
-		self.transform = transform
-		add(animation, forKey: nil)
-	}
-	
-	private func startUpAnimation() {
-		let animation = self.animation
-		animation.fromValue = CATransform3DMakeTranslation(1, translationY, 1)
-		animation.toValue = CATransform3DIdentity
-		activeAnimation = .up
-		transform = CATransform3DIdentity
-		add(animation, forKey: nil)
-	}
-}
-
-extension LoadingViewLayer: CAAnimationDelegate {
-	func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
-		if !flag { return }
-		switch activeAnimation {
-		case .up:
-			startDownAnimation()
-		case .down:
-			startUpAnimation()
-		}
+		circleLayer.add(animation, forKey: nil)
 	}
 }
