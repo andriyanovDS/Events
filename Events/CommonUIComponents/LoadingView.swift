870495a6de38b5445dcce4f05b19dfd7e470e3db
andrianovDS <garro93@gmail.com>
2020-05-11 00:02:23 +0300

    refactor loadingView using CAReplicationLayer

--- Events/CommonUIComponents/LoadingView.swift
+++ Events/CommonUIComponents/LoadingView.swift
@@ -9,63 +9,58 @@
 import UIKit
 
 class LoadingView: UIView {
-	private var viewCenterX: CGFloat = CGFloat.zero
-	
 	private struct Constants {
 		static let animationDuration: TimeInterval = 600
 		static let circlesCount: Int = 3
 		static let circleRadius: CGFloat = 5.0
 	}
-	
-	private func centerX(at index: Int) -> CGFloat {
-		let centerIndex = Constants.circlesCount / 2
-		if index == centerIndex { return viewCenterX }
-		let difference = index - centerIndex
-		return viewCenterX + Constants.circleRadius * 3 * CGFloat(difference)
-	}
-	
+
+  private let replicatorLayer = CAReplicatorLayer()
+  private var circleLayer: CALayer = {
+    let circleLayer = CAShapeLayer()
+    let path = UIBezierPath(
+      arcCenter: CGPoint.zero,
+      radius: Constants.circleRadius,
+      startAngle: 0,
+      endAngle: .pi * 2,
+      clockwise: true
+    )
+    circleLayer.path = path.cgPath
+    circleLayer.fillColor = UIColor.backgroundInverted.cgColor
+    return circleLayer
+  }()
+
+  init() {
+    super.init(frame: CGRect.zero)
+    replicatorLayer.addSublayer(circleLayer)
+    layer.addSublayer(replicatorLayer)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
 	override func draw(_ rect: CGRect) {
-		viewCenterX = rect.width / 2
-		let range = Array([Int](0...Constants.circlesCount - 1))
-		
-		let animation = CABasicAnimation(keyPath: "position")
+    replicatorLayer.frame = CGRect(
+      x: rect.width / 2 - Constants.circleRadius * 4,
+      y: 0,
+      width: Constants.circleRadius * 9,
+      height: Constants.circleRadius * 3
+    )
+    replicatorLayer.instanceCount = Constants.circlesCount
+    let delayInMilliseconds = Int(Constants.animationDuration) / Constants.circlesCount
+    replicatorLayer.instanceDelay = Double(delayInMilliseconds) / 1000
+
+    replicatorLayer.instanceTransform = CATransform3DMakeTranslation(Constants.circleRadius * 3, 0, 0)
+
+		let animation = CABasicAnimation(keyPath: "transform.translation.y")
+    animation.fromValue = 0
+    animation.toValue = Constants.circleRadius * 3
 		animation.autoreverses = true
 		animation.duration = Constants.animationDuration / 1000
-		animation.repeatCount = Float.greatestFiniteMagnitude
-		animation.timingFunction = .linear
-		
-		range
-			.enumerated()
-			.forEach {(index, _) in drawCircle(at: index, withAnimation: animation)}
-	}
+    animation.repeatCount = .infinity
+    animation.timingFunction = .easeInOut
 
-	private func drawCircle(at index: Int, withAnimation animation: CABasicAnimation) {
-		let startPoint = CGPoint(
-			x: centerX(at: index),
-			y: Constants.circleRadius
-		)
-		let endPoint = CGPoint(
-			x: startPoint.x,
-			y: Constants.circleRadius * 3
-		)
-		let delayInMilliseconds = Int(Constants.animationDuration) / Constants.circlesCount * index
-		animation.timeOffset = Double(delayInMilliseconds) / 1000
-		animation.fromValue = startPoint
-		animation.toValue = endPoint
-		
-		let circleLayer = CAShapeLayer()
-		circleLayer.position = startPoint
-		let path = UIBezierPath(
-			arcCenter: CGPoint.zero,
-			radius: Constants.circleRadius,
-			startAngle: 0,
-			endAngle: .pi * 2,
-			clockwise: true
-		)
-		circleLayer.path = path.cgPath
-		circleLayer.fillColor = UIColor.backgroundInverted.cgColor
-		circleLayer.position = startPoint
-		self.layer.addSublayer(circleLayer)
-		circleLayer.add(animation, forKey: nil)
+    circleLayer.add(animation, forKey: "tranlation")
 	}
 }
